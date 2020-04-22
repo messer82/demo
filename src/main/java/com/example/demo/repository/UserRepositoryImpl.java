891a40d51a158aab185f28e071a65ea4420d22e2
messer82 <podinamarius82@gmail.com>
2020-04-22 12:29:33 +0300

    Minor add - application starts now :)

--- src/main/java/com/example/demo/repository/UserRepositoryImpl.java
+++ src/main/java/com/example/demo/repository/UserRepositoryImpl.java
@@ -4,11 +4,13 @@ import com.example.demo.domain.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
 
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+@Component
 public class UserRepositoryImpl implements UserRepository {
 
     @Autowired(required = true)
