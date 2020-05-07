7acacf82f5ed3a2fddc0d4cc9fcf1808b8341ed1
messer82 <podinamarius82@gmail.com>
2020-05-07 14:17:48 +0300

    A few unit tests

--- src/main/java/com/example/demo/FinalProjectApplication.java
+++ src/main/java/com/example/demo/FinalProjectApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //working progress
+//basic mobile banking app
 
 @SpringBootApplication
 public class FinalProjectApplication {
